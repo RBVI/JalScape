a7be9ff0275524d1493a918a769e9e7115b2f0f6
Jim Procter <jprocter@compbio.dundee.ac.uk>
2013-06-28 10:15:01 +0100

    deparse Jalview selections to Cytoscape Network and CySUIDs

--- src/main/java/org/jalview/jalscape/internal/model/CySelectionListener.java
+++ src/main/java/org/jalview/jalscape/internal/model/CySelectionListener.java
@@ -1,14 +1,21 @@
 package org.jalview.jalscape.internal.model;
 
+import jalview.api.AlignViewportI;
+import jalview.datamodel.AlignmentI;
 import jalview.datamodel.ColumnSelection;
 import jalview.datamodel.SequenceGroup;
 import jalview.datamodel.SequenceI;
 import jalview.structure.SelectionListener;
 import jalview.structure.SelectionSource;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.cytoscape.model.CyIdentifiable;
 import org.cytoscape.model.CyNetwork;
@@ -17,8 +24,6 @@ import org.cytoscape.model.CyTable;
 import org.cytoscape.model.events.RowSetRecord;
 import org.cytoscape.model.events.RowsSetEvent;
 import org.cytoscape.model.events.RowsSetListener;
-import org.cytoscape.model.events.RowsCreatedEvent;
-import org.cytoscape.model.events.RowsCreatedListener;
 
 /**
  * This object maintains the selection state between JalView objects and Cytoscape objects.
@@ -70,6 +75,7 @@ public class CySelectionListener implements RowsSetListener, SelectionListener {
 	public void unsilence() {
 		silenced = false;
 	}
+	
 
   /*
    * (non-Javadoc)
@@ -81,25 +87,88 @@ public class CySelectionListener implements RowsSetListener, SelectionListener {
   public void selection(SequenceGroup arg0, ColumnSelection arg1,
           SelectionSource arg2)
   {
+    Map<CyNetwork, Set<CyIdentifiable>> networks = new HashMap<CyNetwork, Set<CyIdentifiable>>();
     if (arg2 != manager)
     {
-      if (arg0 == null || arg0.getSize() == 0)
+      AlignmentI visal = null;
+      if (arg2 instanceof AlignViewportI)
+      {
+        // Selection from specific view
+        visal = ((AlignViewportI) arg2).getAlignment();
+      }
+      if ((arg0 == null || arg0.getSize() == 0)
+              && (arg1 == null || arg1.getSelected() == null || arg1
+                      .getSelected().size() == 0))
       {
         // clear selection;
       }
       else
       {
+        List<CyIdentifiable> cyids = new ArrayList<CyIdentifiable>();
         for (SequenceI sq : arg0.getSequences())
         {
-          SequenceI ds = sq.getDatasetSequence();
+          SequenceI ds = sq;
           while (ds.getDatasetSequence() != null)
           {
             ds = ds.getDatasetSequence();
           }
+          CyIdentifiable cyid = manager.getIdForSeq(ds);
+          if (cyid != null)
+          {
+            Set<CyNetwork> network = manager.getNetworkForId(cyid);
+            if (network != null)
+            {
+              for (CyNetwork netw : network)
+              {
+                Set<CyIdentifiable> netid = networks.get(netw);
+                if (netid == null)
+                {
+                  networks.put(netw, netid = new HashSet<CyIdentifiable>());
+                }
+                netid.add(cyid);
+              }
+            }
+          }
+        }
+
+        if (arg1 != null)
+        {
+          // extract ranges on sq that have been selected
+          if (arg1.getSelected().size() == visal.getHeight())
+          {
+            // code here should we ever need to do this
+            // deparse the colsel into positions on the vamsas alignment
+            // sequences
+            if (arg1.getSelected() != null && arg1.getSelected().size() > 0
+                    && visal != null && arg0.getSize() == visal.getHeight())
+            {
+              // gather selected columns outwith the sequence positions
+              // too
+
+              Enumeration cols = arg1.getSelected().elements();
+              while (cols.hasMoreElements())
+              {
+                int ival = ((Integer) cols.nextElement()).intValue();
+                // ival is a position numbered from 0 in visal's column
+                // coordinates that is selected
+              }
+            }
+            else
+            {
+              int[] intervals = arg1.getVisibleContigs(arg0.getStartRes(),
+                      arg0.getEndRes() + 1);
+              // intervals is a series of start/end positions that are visible
+              // in the alignment view and intersect with the selected rectangle
+              
+            }
+          }
+
           // resolve ds against manager's set of CyIdentifiables and mark the
           // selection
         }
       }
     }
+    cySelect(networks);
   }
+
 }
